010383ea3ff30d7fc96a67fc1ee7dfe58508e195
Azuolas45 <azuolaspavlavicius@gmail.com>
2026-08-16 20:55:29 +0300

    Change cache version from v3 to v4
    Updated cache version to v4 for service worker.

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
    SVARBU: pakeitei index.html? Pakelk CACHE numerį (v3 -> v4),
    kitaip telefonas ir toliau rodys seną versiją. */
-const CACHE = 'maisto-baze-v3';
+const CACHE = 'maisto-baze-v4';
 const FAILAI = [
   './',
   './index.html',
